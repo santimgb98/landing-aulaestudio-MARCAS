440db1890a922b80bd66522444d8a781b06b2769
santimgb98 <santimgb98@gmail.com>
2025-06-17 17:43:02 +0200

    update page and add testimonials

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
 
   return (
     
-   <div>
-      <div className="fixed top-0 navbar bg-blue-500 shadow-sm z-1">
+   <div className="bg-white">
+      <div className="fixed navbar bg-blue-500 shadow-sm z-1">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
@@ -17,7 +17,7 @@ export default function Home() {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-xl dropdown-content bg-blue-500 border-white border-2 rounded-box z-1 mt-5 w-80 p-2 h-80 shadow">
+              className="menu menu-xl dropdown-content bg-blue-500 border-white border-1 rounded-box z-1 mt-5 w-80 p-2 h-80 shadow">
               <li><a>PREINSCRIPCIÓN 25/26</a></li>
               <li><a>Ciclos medios</a></li>
               <li><a>Ciclos superiores</a></li>
@@ -28,28 +28,24 @@ export default function Home() {
           </div>
         </div>
         <div className="navbar-end">
+
           <button className="btn btn-info">
             AULA VIRTUAL
           </button>
-          <button className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
-              <span className="badge badge-xs badge-primary indicator-item"></span>
-            </div>
-          </button>
+
         </div>
       </div>
 
-      <div className="pt-10">
+      <div id="header" className="pt-10 h-250 bg-[url('../pages/images/fondo.jpg')]">
 
         <div className="titulo"></div>
 
-        <h1 className="text-5xl text-center mt-15">
+        <h1 className="text-5xl text-center mt-15 text-white">
           <p>Domina DAM y DAW con Ciclos Superiores y Másteres</p>
           <p className="mt-3">100% Orientados al Empleo</p>
         </h1>
 
-        <h2 className="text-3xl text-center mt-15">
+        <h2 className="text-3xl text-center mt-15 text-white">
           <p>Titulación oficial + prácticas en empresas tech + profesores expertos</p>
           <p className="text-4xl mt-3">¡Conviértete en desarrollador y empieza a trabajar antes de terminar!</p>
         </h2>
@@ -61,27 +57,9 @@ export default function Home() {
           <button className="bg-blue-500 rounded-md w-60 h-20 text-xl skeleton hover:bg-blue-700 hover:scale-110">Preinscripción 25/26</button>
         </div>
       </div>
-
-      <div class="xunta"></div>
-
-      <div className="h-100 pt-10 text-center bg-white text-blue-500">
-
-        <div className="text-3xl mb-10">
-          <p>LOS MEJORES PROGRAMADORES SE FORMARON EN EL CENTRO</p>
-        </div>
-
-        <div className="flex gap-5 justify-evenly">
-          <img className="imgChiqui"></img>
-          <img className="imgLinus"></img>
-          <img className="imgRecio"></img>
-          <img className="imgEvil"></img>
-          <img className="imgGabe"></img>
-        </div>
       
 
-      </div>
-
-      <div className="flex justify-evenly h-130 items-center">
+      <div className="flex justify-evenly w-400 mt-15 mx-auto h-130 items-center bg-blue-500 rounded-xl">
 
         <div class="cursos">
           <p className="text-black text-xl">Certificados Profesionales</p>
@@ -103,69 +81,100 @@ export default function Home() {
 
       </div>
 
-      <div id="contacta" className="content-center h-200 bg-white text-center">
 
-        <p className="text-blue-500 text-5xl"><b>CONTACTA</b></p>
+      <div className="h-100 mt-30 text-center bg-white text-blue-500">
+
+        <div className="text-3xl mb-10">
+          <p>LOS MEJORES PROGRAMADORES SE FORMARON EN ESTE CENTRO</p>
+        </div>
+
+        <div className="flex gap-5 justify-evenly">
+          <img className="imgChiqui"></img>
+          <img className="imgLinus"></img>
+          <img className="imgRecio"></img>
+          <img className="imgEvil"></img>
+          <img className="imgGabe"></img>
+        </div>
+      
+
+      </div>
+
+      <div id="contacta" className=" w-210 mt-15 mx-auto h-180 content-center bg-blue-500 rounded-xl">
+
+        <p className="text-white text-4xl text-center"><b>CONTACTO</b></p>
         
-        <select className="bg-white w-250 m-5 border-blue text-orange-400 select select-info">
+        <select className="bg-white w-200 m-5 border-blue text-orange-400 select select-info">
           <option disabled={true}>MOTIVO</option>
           <option>Consulta sobre ciclo medio</option>
           <option>Consulta sobre ciclo superior</option>
           <option>Consulta sobre ciclo máster</option>
           <option>Otra consulta</option>
         </select>
-        <p></p>
-        <input id="inputNombre" placeholder="NOMBRE" className="text-left p-5 text-orange-400 text-l border-1 border-blue-300 rounded-md w-250"></input>
-        <input id="inputEmail" placeholder="EMAIL" className="text-left p-5 text-orange-400 text-l border-1 border-blue-300 rounded-md w-250 mt-5"></input>
-        <input id="inputTelefono" placeholder="TELÉFONO" className="text-left p-5 text-orange-400 text-l border-1 border-blue-300 rounded-md w-250 mt-5"></input>
-        <input id="inputMensaje" placeholder="MENSAJE" className="text-left p-5 text-orange-400 text-l border-1 border-blue-300 rounded-md w-250 h-40 mt-5"></input>
 
-        <p></p>
-        <button type="button" class="btn text-base-content btn-warning mt-5 hover:bg-white hover:text-warning">ENVIAR</button>
+        <div className="flex justify-center">
+          <input placeholder="NOMBRE" className="bg-white text-left p-5 text-orange-400 text-l border-1 border-blue-300 rounded-md w-200"></input>
+        </div>
+        
+        <div className="flex justify-center">
+          <input placeholder="EMAIL" className="bg-white text-left p-5 text-orange-400 text-l border-1 border-blue-300 rounded-md w-200 mt-5"></input>
+        </div>
+
+        <div className="flex justify-center">
+          <input placeholder="TELÉFONO" className="bg-white text-left p-5 text-orange-400 text-l border-1 border-blue-300 rounded-md w-200 mt-5"></input>
+        </div>
+
+        <div className="flex justify-center">
+          <input placeholder="MENSAJE" className="bg-white text-left p-5 text-orange-400 text-l border-1 border-blue-300 rounded-md w-200 h-40 mt-5"></input>
+        </div>
+
+        <div className="flex justify-center">
+          <button type="button" class="btn text-base-content btn-warning mt-5 hover:bg-white hover:text-warning">ENVIAR</button>
+        </div>
+
       </div>
 
-      <div className="h-140 content-center text-center bg-[url('../pages/images/fondo.jpg')]">
+      <div className="h-100 mt-30 content-center text-center">
 
-        <h1 className="text-5xl mb-8">PROFESORADO</h1>
-
-        <div className="flex justify-evenly text-black">
-          <div className="card bg-orange-300 w-80 shadow-sm pt-5">
+        <div className="flex justify-evenly text-black items-center">
+          <div className="card bg-orange-300 w-80 shadow-sm pt-5 hover:scale-105">
             <figure>
-              <img className="imgEvil"/>
+              <img className="robertoANHQV"/>
             </figure>
             <div className="card-body">
-              <h2 className="card-title justify-center">Ruberto Astro</h2>
-              <p>Profesor de <b>Marcas</b>, <b>Entornos</b> y <b>Sistemas</b>.</p>
-              <p>A Santi le va a subir la nota por esto</p>
+              <h2 className="card-title justify-center ">Ruberto Astro</h2>
+              <p><b>Marcas</b>, <b>Entornos</b> y <b>Sistemas</b>.</p>
               <div className="card-actions justify-center">
-
               </div>
             </div>
           </div>
 
-          <div className="card bg-orange-300 w-80 shadow-sm pt-5">
+          <div className="card bg-orange-300 w-80 shadow-sm pt-5 hover:scale-105">
             <figure>
-              <img className="imgEvil"/>
+              <img className="emilio"/>
             </figure>
             <div className="card-body">
               <h2 className="card-title justify-center">Arturo Vázquez</h2>
-              <p>Profesor de <b>Programación</b></p>
+              <p><b>Programación</b></p>
             </div>
           </div>
 
-          <div className="card bg-orange-300 w-80 shadow-sm pt-5">
+          <div>
+            <h1 className="text-3xl mb-8 text-blue-500">PROFESORADO</h1>
+          </div>
+
+          <div className="card bg-orange-300 w-80 shadow-sm pt-5 hover:scale-105">
             <figure>
-              <img className="imgEvil"/>
+              <img className="isabel"/>
             </figure>
             <div className="card-body">
               <h2 className="card-title justify-center">Tessa Garrido</h2>
-              <p>Profesora de <b>Base de datos</b></p>
+              <p><b>Base de datos</b></p>
             </div>
           </div>
 
-          <div className="card bg-orange-300 w-80 shadow-sm pt-5">
+          <div className="card bg-orange-300 w-80 shadow-sm pt-5 hover:scale-105">
             <figure>
-              <img className="imgEvil"/>
+              <img className="juanCuesta"/>
             </figure>
             <div className="card-body">
               <h2 className="card-title justify-center">Daniel Daniel</h2>
@@ -177,7 +186,7 @@ export default function Home() {
       </div>
 
 
-      <div className="h-120 bg-white content-center">
+      <div className="h-90 bg-white content-center mt-30">
 
         <h1 className="text-blue-500 text-5xl text-center"><b>FAQ</b></h1>
 
@@ -227,9 +236,79 @@ export default function Home() {
 
       </div>
 
-      <div className="h-100">
+      <div className="h-220 mt-30 flex justify-center">
+        <div className="w-300">
+          <h1 className="text-5xl text-blue-500 text-center mb-5"><b>TESTIMONIOS</b></h1>
 
+          <div className="flex justify-evenly gap-5">
+            <div className="m-5">
+              <img className="juanCuesta mb-3"></img>
+              <p className="text-black text-center">Jacinto Veléz</p>
+            </div>
+            <div className="text-black m-5 content-center w-250">
+              <h3>Muy satisfecho</h3>
+              <p>Estoy muy satisfecho con el curso de DAM 
+                (Desarrollo de Aplicaciones Multiplataforma). 
+                Aprendí a programar en Java, Android, SQL y más, 
+                con profesores expertos y prácticas útiles. 
+                Los proyectos me prepararon para el mundo laboral, 
+                y ahora tengo habilidades demandadas. 
+                La organización fue buena, aunque a veces faltaron 
+                recursos técnicos. Lo recomiendo a quienes les 
+                guste la programación y busquen una formación completa. 
+                Gracias a DAM, conseguí un trabajo estable como desarrollador. 
+                ¡Valió totalmente la pena!</p>
+            </div>
+          </div>
+
+          <div className="flex justify-evenly gap-5">
+            <div className="text-black m-5 content-center w-250">
+              <h3>Rompí 4 ventanas</h3>
+              <p>Un acueducto es una obra de ingeniería diseñada para 
+                transportar agua desde un lugar de origen hasta un 
+                punto de distribución, generalmente para abastecer 
+                ciudades o regadíos. Estas estructuras pueden ser 
+                subterráneas, superficiales o elevadas, utilizando 
+                tuberías, canales o arcos de piedra. Los acueductos
+                 han sido fundamentales en el desarrollo de civilizaciones,
+                  como los romanos, quienes construyeron impresionantes
+                   sistemas que aún perduran. Su función es garantizar
+                    el suministro constante de agua, superando 
+                    obstáculos geográficos. Hoy, los acueductos 
+                    modernos emplean tecnología avanzada, pero el 
+                    principio sigue siendo el mismo: llevar agua 
+                    de manera eficiente a donde se necesita.</p>
+            </div>
+            <div className="m-5">
+              <img className="robertoANHQV mb-3"></img>
+              <p className="text-black text-center">Jacinto Veléz</p>
+            </div>
+          </div>
+
+          <div className="flex justify-evenly gap-5">
+            <div className="m-5">
+              <img className="emilio mb-3"></img>
+              <p className="text-black text-center">Jacinto Veléz</p>
+            </div>
+            <div className="text-black m-5 content-center">
+              <h3>Estoy encerrado</h3>
+              <p>Llevo desde que empezó el curso escolar encerrado en el baño.
+                Escribo por aquí por desesperación; ya casi no me queda papel 
+                higiénico para comer, AYUDA
+              </p>
+            </div>
+          </div>
+          
+
+          
+        </div>
       </div>
+
+      <footer className="footer sm:footer-horizontal footer-center bg-blue-500 text-base-content p-4">
+        <aside>
+          <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+        </aside>
+      </footer>
 
     </div>
     
